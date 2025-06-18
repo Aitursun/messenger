2d712f99480c8433174f95ca711f8c3f270cb67f
Les <adiletibraev98@gmail.com>
2025-06-19 02:17:10 +0600

    2

--- config.py
+++ config.py
@@ -1,4 +1,9 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()  # Для локальной разработки
+
 class Config:
-    SECRET_KEY = 'your-secret-key'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://messenger_prod_v2_user:7cjueyX9t68N3xD0C1PpWb5sU79fFUsG@dpg-d19h2rgdl3ps73cln59g-a.oregon-postgres.render.com/messenger_prod_v2'
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
