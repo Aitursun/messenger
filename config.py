d01c222302ebc9dc4f2c4ea5d4b9a429891a1d52
Les <adiletibraev98@gmail.com>
2025-06-19 02:07:34 +0600

    Initial commit

---
+++ config.py
@@ -0,0 +1,4 @@
+class Config:
+    SECRET_KEY = 'your-secret-key'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://messenger_prod_v2_user:7cjueyX9t68N3xD0C1PpWb5sU79fFUsG@dpg-d19h2rgdl3ps73cln59g-a.oregon-postgres.render.com/messenger_prod_v2'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
